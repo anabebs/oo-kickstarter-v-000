0497924761fbb4b54bb75237de8406c3477a790f
Ana Bebic <anabebic.x@gmail.com>
2018-07-17 15:36:25 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,14 @@
 class Project
-  attr_accessor :title
-  
-  
-  def initialize(title)
-    @title =title
-    
-  end 
-  
-end
+ attr_accessor :backers
+  attr_reader :title
+
+  def initialize(project_name)
+    @backers = []
+    @title = project_name
+  end
+
+  def add_backer(backer)
+    backer.backed_projects << self
+    backers << backer
+  end
+end 
